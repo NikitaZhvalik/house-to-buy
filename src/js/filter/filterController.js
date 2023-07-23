5411f7890ad935fffcc66f5d3de714258d9b9860
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-07-23 15:02:15 +1000

    05: выводим параметры фильтра с помощью js

--- src/js/filter/filterController.js
+++ src/js/filter/filterController.js
@@ -1,10 +1,11 @@
 import Filter from './filterModel';
 import * as view from './filterView';
 
-export default function(state) {
-    view.render();
+export default async function(state) {
     // создаем объект фильтра
     if (!state.filter) state.filter = new Filter();
     //получаем параметры для фильтра
-    state.filter.getParams();
+    await state.filter.getParams();
+    //отрисовка фильтра
+    view.render(state.filter.params);
 }
--- src/js/filter/filterView.js
+++ src/js/filter/filterView.js
@@ -1,4 +1,17 @@
-export function render() {
+export function render(params) {
+
+    let complexNames = '';
+    params.complexNames.forEach( name => complexNames += `<option value="${name}">ЖК ${name}</option>`);
+
+    let rooms = '';
+    params.roomValues.forEach( value => rooms += `  <input
+                                                        name="rooms"
+                                                        type="checkbox"
+                                                        id="rooms_${value}"
+                                                        class="rooms__checkbox"
+                                                        value="${value}"/>
+                                                        <label for="rooms_${value}" class="rooms__btn">${value}</label>`);
+    
     const html = `
     <form method="GET" class="container p-0">
         <div class="heading-1">Выбор квартир:</div>
@@ -7,53 +20,13 @@ export function render() {
                 <div class="filter__label">Выбор проекта:</div>
                 <select name="complex" id="" class="filter__dropdown">
                     <option value="all">Все проекты</option>
-                    <option value="Генеральский"
-                        >ЖК Генеральский</option
-                    >
-                    <option value="Речной">ЖК Речной</option>
-                    <option value="Лесной">ЖК Лесной</option>
-                    <option value="Квантум">ЖК Квантум</option>
+                    ${complexNames}
                 </select>
             </div>
             <div class="filter__col rooms">
                 <div class="filter__label">Комнат:</div>
                 <div class="rooms__wrapper">
-                    <input
-                        name="rooms"
-                        type="checkbox"
-                        id="rooms_1"
-                        class="rooms__checkbox"
-                        value="1"
-                    /><label for="rooms_1" class="rooms__btn">1</label>
-                    <input
-                        name="rooms"
-                        type="checkbox"
-                        id="rooms_2"
-                        class="rooms__checkbox"
-                        value="2"
-                        checked
-                    /><label for="rooms_2" class="rooms__btn">2</label>
-                    <input
-                        name="rooms"
-                        type="checkbox"
-                        id="rooms_3"
-                        class="rooms__checkbox"
-                        value="3"
-                    /><label for="rooms_3" class="rooms__btn">3</label>
-                    <input
-                        name="rooms"
-                        type="checkbox"
-                        id="rooms_4"
-                        class="rooms__checkbox"
-                        value="4"
-                    /><label for="rooms_4" class="rooms__btn">4</label>
-                    <input
-                        name="rooms"
-                        type="checkbox"
-                        id="rooms_5"
-                        class="rooms__checkbox"
-                        value="5"
-                    /><label for="rooms_5" class="rooms__btn">5</label>
+                    ${rooms}
                 </div>
             </div>
             <div class="filter__col">
@@ -66,7 +39,8 @@ export function render() {
                             min="0"
                             type="number"
                             class="range__input"
-                            placeholder="38"
+                            placeholder="${params.squareMin}"
+                            value = "${params.squareMin}"
                         />
                         <div class="range__value">м2</div>
                     </label>
@@ -77,7 +51,8 @@ export function render() {
                             min="0"
                             type="number"
                             class="range__input"
-                            placeholder="120"
+                            placeholder="${params.squareMax}"
+                            value = "${params.squareMax}"
                         />
                         <div class="range__value">м2</div>
                     </label>
@@ -93,7 +68,8 @@ export function render() {
                             name="pricemin"
                             min="0"
                             class="range__input range__input--price"
-                            placeholder="2325000"
+                            placeholder="${params.priceMin}"
+                            value = "${params.priceMin}"
                         />
                         <div class="range__value">₽</div>
                     </div>
@@ -104,7 +80,8 @@ export function render() {
                             name="pricemax"
                             min="0"
                             class="range__input range__input--price"
-                            placeholder="4525000"
+                            placeholder="${params.priceMax}"
+                            value = "${params.priceMax}"
                         />
                         <div class="range__value">₽</div>
                     </div>
@@ -117,5 +94,6 @@ export function render() {
         </div>
     </form>
     `;
+
     document.querySelector('#app').insertAdjacentHTML('afterbegin', html);
 }
