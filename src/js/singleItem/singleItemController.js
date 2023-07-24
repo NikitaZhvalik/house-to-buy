8fc1c2ac6f584dd4cd0e4b51370de16c811df4ef
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-07-24 15:55:37 +1000

    16: рендер шаблона для объекта

--- src/js/singleItem/singleItemController.js
+++ src/js/singleItem/singleItemController.js
@@ -1,8 +1,9 @@
 import SingleItem from './singleItemModel'
+import * as view from './singleItemView';
 
 export default function () {
     // передаем id
     state.singleItem = new SingleItem(state.routeParams );
     state.singleItem.getItem();
-
+    view.render();
 }
---
+++ src/js/singleItem/singleItemView.js
@@ -0,0 +1,102 @@
+export function render() {
+    const appContainer = document.querySelector('#app');
+
+    const html = `
+            <div class="container p-0 pt-5">
+                <div class="heading-1">
+                    Квартира Студия, 45,5 м2 за 4 250 000 ₽
+                </div>
+
+                <!-- object -->
+                <div class="object">
+                    <!-- object__photo -->
+                    <div class="object__photo">
+                        <div class="object__photo-wrapper">
+                            <img src="img/slider-item/photo.png" alt="" />
+                        </div>
+                    </div>
+                    <!-- // object__photo -->
+
+                    <!-- object__desc -->
+                    <div class="object__desc">
+                        <div class="object__desc-sector">
+                            ЖК Генеральский
+                        </div>
+
+                        <div class="object__desc-name">
+                            <div class="object__desc-title">
+                                Студия, 45,5 м2
+                            </div>
+                            <div class="object__desc-art">ГЕН-112-42</div>
+
+                            <!-- Добавить в избранное -->
+                            <button class="button-favourite">
+                                <i class="fas fa-heart"></i> <span>В избранное</span>
+                            </button>
+
+                            <!-- В Избранном -->
+                            <button class="button-favourite button-favourite--active">
+                                <i class="fas fa-heart"></i> <span>В избранном</span>
+                            </button>
+
+                        </div>
+
+                        <div class="object__desc-details">
+                            <!-- params -->
+                            <div class="params">
+                                <div class="params__item">
+                                    <div class="params__definition">Корпус</div>
+                                    <div class="params__value">3</div>
+                                </div>
+                                <div class="params__item">
+                                    <div class="params__definition">Этаж</div>
+                                    <div class="params__value">8</div>
+                                </div>
+                                <div class="params__item">
+                                    <div class="params__definition">Номер</div>
+                                    <div class="params__value">121</div>
+                                </div>
+                                <div class="params__item">
+                                    <div class="params__definition">Комнат</div>
+                                    <div class="params__value">2</div>
+                                </div>
+                            </div>
+                            <!-- // params -->
+                        </div>
+
+                        <div class="details">
+                            <div class="details__row">
+                                <div class="details__name">Стоимость</div>
+                                <div
+                                    class="details__value details__value--price"
+                                >
+                                    4 200 000 ₽
+                                </div>
+                            </div>
+                            <div class="details__row">
+                                <div class="details__name">Цена за м2</div>
+                                <div class="details__value">64 000 ₽/м2</div>
+                            </div>
+                            <div class="details__row">
+                                <div class="details__name">Площадь</div>
+                                <div class="details__value">60 м2</div>
+                            </div>
+                        </div>
+
+                        <button class="button-order">Забронировать</button>
+                        <!-- <button class="button-preview">Записаться на просмотр</button> -->
+                    </div>
+                    <!-- // object__desc -->
+                </div>
+                <!-- // object -->
+            </div>
+
+            <div class="container p-0">
+                <a href="index.html" class="back-to-results"
+                    >← Вернуться к результатам поиска</a
+                >
+            </div>    
+    `
+
+    appContainer.insertAdjacentHTML('beforeend', html);
+}
